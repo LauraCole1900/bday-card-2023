7f798eeb49455d096e332e4be45676b7995b4d52
Laura Cole <lauracole1900@comcast.net>
2023-10-10 02:38:47 -0600

    gh-pages deploy?

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/bday-card-2023",
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
   server: {
     port: 3000,
